f8876421708ee83242189898603ba771824376a1
7FM <41307817+7FM@users.noreply.github.com>
2021-07-09 14:44:47 +0200

    first round of patches for the new cva6 packaging

--- riscv/cva6/setup.sh
+++ riscv/cva6/setup.sh
@@ -27,27 +27,12 @@ export RV_ROOT=$(pwd)
 #mv clint_top.v clint_top.sv
 
 vivado -nolog -nojournal -mode batch -source ../package.tcl
-#vivado -nolog -nojournal -mode batch -source ../package_timer.tcl
-#vivado -nolog -nojournal -mode batch -source ../package_dm.tcl
 
 if [ ! -d "../../../IP/riscv/CVA6/cva6" ]; then
     mkdir -p "../../../IP/riscv/CVA6/cva6"
     echo "Unzipping CVA6..."
     unzip risc-v_cva6.zip -d ../../../IP/riscv/CVA6/cva6
 fi
-
-#if [ ! -d "../../../IP/riscv/CVA6/timer" ]; then
-#    mkdir -p "../../../IP/riscv/CVA6/timer"
-#    echo "Unzipping CVA6 Timer..."
-#    unzip risc-v_cva6_timer.zip -d ../../../IP/riscv/CVA6/timer
-#fi
-
-#if [ ! -d "../../../IP/riscv/CVA6/dm" ]; then
-#    mkdir -p "../../../IP/riscv/CVA6/dm"
-#    echo "Unzipping CVA6 Debug Module..."
-#    unzip risc-v_cva6_dm.zip -d ../../../IP/riscv/CVA6/dm
-#fi
-
 
 cd ../../..
 #cd ../..
